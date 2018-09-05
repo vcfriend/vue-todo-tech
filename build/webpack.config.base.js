aa3ef98a9fdfd4a2935c81ec0b5776f5b042c88b
wb-xyl431344 <wb-xyl431344@alibaba-inc.com>
2018-09-05 10:05:41 +0800

    修改publicPath端口号问题得到解决了
    修改base.js配置publicPath端口号跟practice.js配置的端口保持一致,问题得到时解决

--- build/webpack.config.base.js
+++ build/webpack.config.base.js
@@ -9,7 +9,7 @@ const config = {
   output: {
     filename: 'bundle.[hash:8].js',
     path: path.join(__dirname, '../public'),
-    publicPath: 'http://127.0.0.1:8000/public/'
+    publicPath: 'http://127.0.0.1:8080/public/'
   },
   module: {
     rules: [
