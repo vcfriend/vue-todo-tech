5b4f2ae6d8defa81ba67998888c225dea302073c
yalin <vcfriend@qq.com>
2018-09-09 21:23:56 +0800

    Revert "Revert "3-5 computed和watch使用场景和方法""
    This reverts commit ce5621c5be0dbf5b103e43aafb7324fd11d1999a.

--- practice/index.js
+++ practice/index.js
@@ -1,11 +1,6 @@
 // import './instance/index2'
 // import './lifecycle/index2'
-<<<<<<< HEAD
-import './data-binding/index2'
-// import './computed'
-=======
 // import './data-binding/index2'
 import './computed/index2'
->>>>>>> yalin1
 // import './directive'
 // import './component'
